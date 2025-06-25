4e935ace9835e7e00cf05d0d40aeb6e601ee6389
yuka <advice5540@gmail.com>
2025-06-25 14:08:38 +0800

    build: nextjs static export project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
+}
 
 export default nextConfig;
